935195b0141a7417ccda0035c26c74e655fc9281
Keigo Imai <keigo.imai@gmail.com>
2021-10-13 15:07:57 +0900

    adjust to the paper version

--- test/paper/test.ml
+++ test/paper/test.ml
@@ -1,65 +1,35 @@
-(* don't make preprocessor warnings as errors *)
-[@@@warnerror "-22"]
+let KMC (uch,mch,wch) = [%kmc.gen (u,m,w)]
 
-open Kmclib
+let rec f n = (* omitted expensive function *) if n = 0 then 0  else if n = 1 then 1  else f (n-2) + f (n-1)
 
-let KMC (uch,mch,wch) = [%kmc.gen fib (u,m,w)]
-
-let rec fib n =
-  if n = 0 then 0
-  else if n = 1 then 1
-  else fib (n-2) + fib (n-1)
-
-let user () =
+let user () : unit =
   let uch = send uch#m#compute 20 in
   let `result(res, uch) = receive uch#m in
   Printf.printf "result: %d\n" res;
-  close (send uch#m#stop ())
+  send uch#m#stop ()
 
-let master () =
-  let rec loop (mch : [%kmc.check fib.m]) =
+let worker () : unit =
+  let rec loop (wch : [%kmc.check w]) =
+    match receive wch#m with
+    | `task(num, wch) ->
+      loop (send wch#m#result (f num))
+    | `stop((), wch) -> wch
+  in loop wch
+let master () : unit =
+  let rec loop (mch : [%kmc.check m]) =
     match receive mch#u with
     | `compute(x, mch) ->
       let mch = send mch#w#task (x / 2) in
       let mch = send mch#w#task (x / 4) in
-      let y = fib x in
       let `result(r1, mch) = receive mch#w in
       let `result(r2, mch) = receive mch#w in
-      loop (send mch#u#result (y + r1 + r2))
+      loop (send mch#u#result (r1 + r2))
     | `stop((), mch) ->
-      close (send mch#w#stop ())
-  in
-  loop mch
-
-let worker () =
-  let rec loop (wch : [%kmc.check fib.w]) =
-    match receive wch#m with
-    | `task(num, wch) ->
-      loop (send wch#m#result (fib num))
-    | `stop((), wch) ->
-      close wch
-  in
-  loop wch
+      send mch#w#stop ()
+  in loop mch
 
 let () =
   let ut = Thread.create user () in
   let mt = Thread.create master () in
   let wt = Thread.create worker () in
   List.iter Thread.join [ut;mt;wt]
-
-
-(*
-  let g = fix(fun t -> 
-      choice_at u ##
-      (u, (u --> m) compute @@
-          (m --> w) task @@
-          (w --> m) result @@
-          (m --> w) task @@
-          (w --> m) result @@
-          (m --> u) result @@
-          t)
-      (u, (u --> m) stop @@
-          (m --> w) stop @@
-          finish))
-    )
-*)
