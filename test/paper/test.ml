cdaf758f3598e6d287c3e1a2bfd04dba09473e68
Keigo Imai <keigo.imai@gmail.com>
2021-10-14 11:33:15 +0900

    paper example: loop unroll worker

--- test/paper/test.ml
+++ test/paper/test.ml
@@ -7,26 +7,23 @@ let user () : unit =
   let `result(res, uch) = receive uch#m in
   Printf.printf "result: %d\n" res;
   send uch#m#stop ()
-
-let worker () : unit =
-  let rec loop (wch : [%kmc.check w]) =
+let worker () =
+  let rec loop (wch : [%kmc.check w]) : unit =
     match receive wch#m with
     | `task(num, wch) ->
       loop (send wch#m#result (f num))
     | `stop((), wch) -> wch
   in loop wch
 let master () : unit =
-  let rec loop (mch : [%kmc.check m]) =
-    match receive mch#u with
-    | `compute(x, mch) ->
-      let mch = send mch#w#task (x / 2) in
-      let mch = send mch#w#task (x / 4) in
-      let `result(r1, mch) = receive mch#w in
-      let `result(r2, mch) = receive mch#w in
-      loop (send mch#u#result (r1 + r2))
-    | `stop((), mch) ->
-      send mch#w#stop ()
-  in loop mch
+  let (mch : [%kmc.check m]) = mch in
+  let `compute(x, mch) = receive mch#u in
+  let mch = send mch#w#task (x / 2) in
+  let mch = send mch#w#task (x / 4) in
+  let `result(r1, mch) = receive mch#w in
+  let `result(r2, mch) = receive mch#w in
+  let mch = send mch#u#result (r1 + r2) in
+  let `stop((), mch) = receive mch#u in
+  send mch#w#stop ()
 
 let () =
   let ut = Thread.create user () in
